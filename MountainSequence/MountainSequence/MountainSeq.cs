ca503d64be43962a8d9764eb8454b9492d4b1043
Surajit Ghosh <Surajit.1986@gmail.com>
2023-09-09 10:49:54 +0530

    Leetcode valid mountain sequence problem

---
+++ MountainSequence/MountainSequence/MountainSeq.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MountainSequence
+{
+    internal class MountainSeq
+    {
+        public bool IfMountainSeq(int[] arr)
+        {
+            int i = 1;
+
+            while((i<arr.Length) && (arr[i-1] < arr[i]))
+            {
+                i++;
+            }
+
+            if((i ==1) || (i ==arr.Length)) { return false; }
+
+            while((i < arr.Length) && (arr[i-1] > arr[i]))
+            {
+                i++;
+            }
+
+            if(i == arr.Length) {  return true; }
+            return false;
+        }
+    }
+}
---
+++ MountainSequence/MountainSequence/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MountainSequence;
+
+Console.WriteLine("check mountain sequence for given array : {0,2,3,4,5,2,1}");
+var arr = new int[] { 0, 2, 3, 4, 5, 2, 1 };
+MountainSeq obj = new MountainSeq();
+Console.WriteLine(obj.IfMountainSeq(arr));
+Console.ReadKey();
